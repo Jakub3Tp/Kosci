02bd9b0595bf19a8b0372f8a40b0898cfd36b7e9
5TpNowickaMonika <egzamin@poczta.pl>
2025-10-27 09:33:47 +0100

    27.10.2025

---
+++ app/src/main/java/com/example/mobilne/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.mobilne;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView[] DiceImages;
+    private
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextView thisTime_result = findViewById(R.id.thisTime_result);
+        TextView result = findViewById(R.id.result);
+
+        DiceImages = [
+            findViewById(R.id.imageView1),
+            findViewById(R.id.imageView2),
+            findViewById(R.id.imageView3),
+            findViewById(R.id.imageView4),
+            findViewById(R.id.imageView5)
+        ]
+
+
+        Button Throw = findViewById(R.id.button_throw);
+        Button Reset = findViewById(R.id.button_reset);
+
+        Throw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
